42551d7bbe23106808e813add86d26b6472e0983
Gustavo Henrique Pinto <ghp2201@gmail.com>
2025-07-15 22:24:03 -0300

    improve: add middlewares

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import TracingApi from './features/tracing/api'
 import PageVisitUseCases from './features/page_visit/use_cases'
 import PageVisitApi from './features/page_visit/api'
+import { logger } from 'hono/logger'
+import { trimTrailingSlash } from 'hono/trailing-slash'
+import { requestId } from 'hono/request-id'
+import { uuidv7 } from 'uuidv7';
 
 const db = drizzle(process.env.DSN)
 
@@ -12,8 +16,15 @@ const pageVisitUseCases = new PageVisitUseCases(db)
 const pageVisitApi = new PageVisitApi(pageVisitUseCases)
 
 const app = new Hono()
-  .notFound((c) => c.json({"type": "errorNotFound", "message": "page does not exists"}))
-  .onError((err, c) => c.json({"type": "errorInternal", "message": err.message}))
+  .use(
+    logger(),
+    trimTrailingSlash(),
+    requestId({
+      generator: (_) => uuidv7()
+    })
+  )
+  .notFound((c) => c.json({"type": "errorNotFound", "message": "page does not exists"}, 404))
+  .onError((err, c) => c.json({"type": "errorInternal", "message": err.message}, 422))
   .route("/", tracingApi.routes())
   .route("/", pageVisitApi.routes())
 
